d03967a79bcce9ca7f9c964233d1fc998df81f2f
Wiktoria <wiktoria.janska@outlook.com>
2023-04-24 15:25:58 +0200

    selecting features test

--- msiProjekt/train.py
+++ msiProjekt/train.py
@@ -1,15 +1,33 @@
 import numpy as np
+import pandas as pd
 import warnings
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
+from matplotlib import figure
+import seaborn as sns
 from LogisticRegression import LogisticRegression
 
 # supress warnings
 warnings.filterwarnings('ignore')
 
-bc = datasets.load_breast_cancer()
-X, y = bc.data, bc.target
+# bc = datasets.load_breast_cancer()
+# X, y = bc.data, bc.target
+
+data = pd.read_csv('train.csv')
+
+# Dividing data into features (X) and labels (y)
+X = data.drop(['Survived'], axis=1)
+y = data['Survived']
+
+X = pd.get_dummies(X, prefix_sep='_')
+y = LabelEncoder().fit_transform(y)
+X = StandardScaler().fit_transform(X)
+
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -23,6 +41,9 @@ plt.show()
 clf = LogisticRegression(lr=0.01)       # Classifier
 clf.fit(X_train, y_train)
 y_predictions = clf.predict(X_test)
+
+print(confusion_matrix(y_test, y_predictions))
+print(classification_report(y_test, y_predictions))
 
 def accuracy(y_predictions, y_test):
     return np.sum(y_predictions==y_test)/len(y_test)
