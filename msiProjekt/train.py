d990b04d773255b192ac395131ebfc0dc7f84ad1
Wiktoria <wiktoria.janska@outlook.com>
2023-05-02 10:44:08 +0200

    save and load to files

--- msiProjekt/train.py
+++ msiProjekt/train.py
@@ -34,7 +34,10 @@ X = imputer.fit_transform(X)
 
 # feature selection
 selector = SelectKBest(score_func=f_classif, k=5)
-X = selector.fit_transform(X, y)
+X_selected = selector.fit_transform(X, y)
+
+# save selected features to file
+np.save('selected_features.npy', X_selected)
 
 # initialize n_splits and n_repeats
 n_splits = 2
@@ -42,14 +45,20 @@ n_repeats = 5
 rkf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=42)
 
 # create a classifier
-clf = LogisticRegression(lr=0.0001, n_iters=1000)
+clf = LogisticRegression(lr=0.001, n_iters=1000)
 
 # perform cross-validation
-scores = cross_val_score(clf, X, y, cv=rkf)
+scores = cross_val_score(clf, X_selected, y, cv=rkf)
+
+# save cross-validation results to file
+np.save('cross_validation_scores.npy', scores)
 
 # train and predict with logistic regression on full data
-clf.fit(X, y)
-y_predictions = clf.predict(X)
+clf.fit(X_selected, y)
+y_predictions = clf.predict(X_selected)
+
+# save predictions to file
+np.save('predictions.npy', y_predictions)
 
 # unclassified data
 plt.scatter(data['Age'], y, color="b", marker="o", s=30)
@@ -75,5 +84,23 @@ acc = accuracy(y_predictions, y)
 print("Accuracy: ", acc)
 
 # print classification report and confusion matrix
+print(confusion_matrix(y, y_predictions))
+print(classification_report(y, y_predictions))
+
+# load saved files and analyze results
+selected_features = np.load('selected_features.npy')
+scores = np.load('cross_validation_scores.npy')
+y_predictions = np.load('predictions.npy')
+
+# print selected features
+print("Selected features:")
+print(selected_features)
+
+# print cross-validation scores
+print("Cross-validation scores:")
+print(scores)
+
+# print classification report and confusion matrix for predictions
+print("Classification report and confusion matrix for predictions:")
 print(confusion_matrix(y, y_predictions))
 print(classification_report(y, y_predictions))
